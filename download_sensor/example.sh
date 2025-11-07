292465e6df822b3e4b3b72a267e786f3c1df7f9f
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-11-07 11:22:43 +1000

    Add example script for downloading sensors

---
+++ download_sensor/example.sh
@@ -0,0 +1,11 @@
+# Download Linux sensor
+./download_cb_sensor.sh 192.168.1.30 YOUR_API_TOKEN linux
+
+# Download Windows EXE sensor
+./download_cb_sensor.sh 192.168.1.30 YOUR_API_TOKEN windows-exe
+
+# Download Windows MSI sensor for group 2
+./download_cb_sensor.sh 192.168.1.30 YOUR_API_TOKEN windows-msi 2
+
+# Download Mac sensor with custom filename
+./download_cb_sensor.sh 192.168.1.30 YOUR_API_TOKEN osx 1 my-sensor.zip
